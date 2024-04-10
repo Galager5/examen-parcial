5f719cf67a123db5b914bb8d523a0fe1242af147
Galager5 <201804890H@utea.edu.pe>
2024-04-10 11:48:40 -0500

    se agrega mas cosas

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace examen_parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(859, 519);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSueldoBruto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnTotalSueldos;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SueldoBruto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Categoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SueldoNeto;
+    }
+}
+
